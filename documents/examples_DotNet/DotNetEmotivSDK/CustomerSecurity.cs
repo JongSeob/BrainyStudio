48c4eb8e1a1e5549a1914d9b1be714121e0b076b
Ondřej Řeháček <Ondrejrehacek@gmail.com>
2017-05-31 20:19:17 +0900

    Project cleanup.

---
+++ documents/examples_DotNet/DotNetEmotivSDK/CustomerSecurity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Emotiv
+{
+    public class CustomerSecurity
+    {
+
+        [DllImport("edk.dll", EntryPoint = "EE_GetSecurityCode")]
+        static extern Double Unmanged_EE_GetSecurityCode();
+        [DllImport("edk.dll", EntryPoint = "EE_CheckSecurityCode")]
+        static extern Int32 Unmanged_EE_CheckSecurityCode(Double x);
+        [DllImport("CustomerSecurity.dll", EntryPoint = "emotiv_func")]
+        static extern Double Unmanged_emotiv_func(Double x);
+
+        public static Double EE_GetSecurityCode()
+        {
+            return Unmanged_EE_GetSecurityCode();
+        }
+        public static Int32 EE_CheckSecurityCode(Double x)
+        {
+            return Unmanged_EE_CheckSecurityCode(x);
+        }
+        public static Double emotiv_func(Double x)
+        {
+            return Unmanged_emotiv_func(x);
+        }
+    }
+}
--- src/api/Controllers/Recording/RecordingController.cs
+++ src/api/Controllers/Recording/RecordingController.cs
@@ -3,6 +3,7 @@ using Sdk.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Web;
 using System.Web.Http;
 
@@ -30,30 +31,30 @@ namespace api.Controllers
         // POST: api/Record
         public void Post()
         {
-            // Open connection
             // Recieve the file and save it to the temporary folder
             var request = HttpContext.Current.Request;
-            var filePath = "/recordings/" + request.Headers["filename"];
+            var filePath = "E:/temp_" + DateTime.Now.Millisecond.ToString();
             using (var fs = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
             {
                 request.InputStream.CopyTo(fs);
             }
 
-            // Close connection
-
             // ASYNC:
-
-            // If compressed unzip the temporary file first
-
-            // Read a first 100kb of the file to get metadata
 
             // Form a metadata and input them into database
 
+
             // Move file into particular folder
 
+
             // Delete temporary data
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
         }
 
+        // TODO
         // PUT: api/Record/5
         public void Put(int id, [FromBody]Recording value)
         {
@@ -62,6 +63,25 @@ namespace api.Controllers
         // DELETE: api/Record/5
         public void Delete(int id)
         {
+            //Get User ID and form SQL Comand
+            int userId = Convert.ToInt32(HttpContext.Current.User.Identity.Name);
+            string strSql = "DELETE FROM Recording WHERE Id = @RecordingId AND User_ID = @UserId";
+
+            //If user is admin list all repositories no matter what user is the owner
+            if (HttpContext.Current.User.IsInRole("Administrator"))
+                strSql = "DELETE FROM Recording WHERE Id = @RecordingId";
+
+            //Open MSSQL Connection and delete data
+            _databaseConnection.Open();
+            using (_databaseConnection)
+            {
+                using (SqlCommand sqlCommand = new SqlCommand(strSql, _databaseConnection))
+                {
+                    sqlCommand.Parameters.Add("@RecordingId", id);
+                    sqlCommand.Parameters.Add("@UserId", userId);
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
